93fab7cd9e893f0b4f097b101071979effa1f7a3
mcdre <mcdre@DESKTOP-2FMHK21>
2018-04-27 22:52:43 -0600

    agregacion de la clase gestor compra

---
+++ src/marilion/GestorCompra.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+/**
+ *
+ * @author mcdre
+ */
+public class GestorCompra {
+    private int tipoPago;
+    private float monto;
+    public Persona cliente;
+}
