2775fccaf8d1b99698c32843d77d986c4321aadd
yury_ <yury_@DESKTOP-9KLH410>
2018-04-27 22:59:43 -0600

    adding classes GestorBase

---
+++ src/marilion/GestorBase.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+
+/**
+ *
+ * @author yury_
+ */
+public class GestorBase {
+    private Huesped Db_HuespedesActivos;
+    private Huesped Db_ReservaHist;
+    private Reservacio reservacion;
+    
+}
