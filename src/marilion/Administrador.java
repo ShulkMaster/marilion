d30b755db6c712763a69d44e93193ba649f76588
yury_ <yury_@DESKTOP-9KLH410>
2018-04-27 21:40:36 -0600

    agregando clases de base y clases de prueba para el repositorio. commit de prueba, ignorece

---
+++ src/marilion/Administrador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+/**
+ *
+ * @author yury_
+ */
+public class Administrador  extends Persona{
+    
+    public String username;
+    private String password;
+    
+}
---
+++ src/marilion/Marilion.java
@@ -0,0 +1,14 @@
+package marilion;
+
+/**
+ *
+ * @author yury_
+ */
+public class Marilion {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("xd");
+    }
+    
+}
