bbe07457f3be3ac12f6732e80f6cbfb0b100f3d4
mcdre <mcdre@DESKTOP-2FMHK21>
2018-04-27 22:59:41 -0600

    agregacion de la clase Habitacion

---
+++ src/marilion/Habitacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+/**
+ *
+ * @author mcdre
+ */
+
+import java.util.ArrayList;
+
+public class Habitacion {
+    public char indicadorDePiso;
+    public int numeroHabitacion;
+    public int habitacionEstado;
+    public String tipo;
+    public int piso;
+    public ArrayList<Huesped> listaHuesped; 
+}
