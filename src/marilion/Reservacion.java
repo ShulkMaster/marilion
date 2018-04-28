60e15cca225413c291e2fadc6fb635ae30c60000
GerryMG <gerardo.andresz4@live.com>
2018-04-27 23:12:21 -0600

    Creacion clase Reservacion

---
+++ src/marilion/Reservacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author gerar
+ */
+public class Reservacion {
+    ArrayList<Habitacion> ListaHabitacionR;
+    ArrayList<Persona> ListaPersonas;
+    int NumeroDeHabitacion;
+    EstadoReservacion Estado;
+    int Dias;
+    int Id_Reservacion;
+    Persona PersonaAPagar;
+    Date fechaIni;
+    
+    
+    
+}
