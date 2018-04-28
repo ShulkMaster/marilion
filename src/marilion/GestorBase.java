5265103ea069cccb4d0c872719e484532117aaf2
GerryMG <gerardo.andresz4@live.com>
2018-04-27 23:54:35 -0600

    se agrego u ncomentario a gestor de base para agregar los achivos en vez de las listas.

--- src/marilion/GestorBase.java
+++ src/marilion/GestorBase.java
@@ -11,6 +11,8 @@ package marilion;
  * @author yury_
  */
 public class GestorBase {
+    //aqui deben ir los archivos de la base de datos, 4 archivos. 
+    //Hay que cambiar y agregar la lista de factura
     private Huesped Db_HuespedesActivos;
     private Huesped Db_ReservaHist;
     private Reservacion reservacion;
