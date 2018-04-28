64b77739a488dc22e3dbeff7925c8447aba9aebe
shulkmaster <yury_@DESKTOP-9KLH410>
2018-04-28 01:30:46 -0600

    esta es mi propuesta de menu xd de momento no hace nada pero ya se puede navegar, solo que tiene un bug y como ya es de madrugada no lo quiero arreglar no me da el seso. la cosa es que cuando pasas de un submenu a un menu principal te dice adios se sale del sub menu y luego imprime el error de opcion no valida pero no se porque. pero la navegacion es 100% funcional

--- src/marilion/Marilion.java
+++ src/marilion/Marilion.java
@@ -4,11 +4,21 @@ package marilion;
  *
  * @author yury_
  */
+/*
+1)llevar el control de las reservas
+2)administrar habitaciones
+3)información personal de sus huéspedes
+    número de la habitación
+    cuántos días ha reservado la habitación
+    costo por noche de la habitación 
+    el total que se le cobrará a su tarjeta de crédito. 
+
+ */
 public class Marilion {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("xd");
+        Menu.Init();
+        Menu.menu.Start();
     }
-    
+
 }
--- src/marilion/Menu.java
+++ src/marilion/Menu.java
@@ -10,21 +10,153 @@ package marilion;
  * @author yury_
  */
 public class Menu {
-    GestorHotel gestorHotel;
+
+    //controla el while principal del programa
+    private Boolean alive;
+    private GestorHotel gestorHotel;
+    private int opcionSelected;
+    //son submenus del menu y recibe un array de cadenas
+    private final tabes[] submenus = new tabes[3];
     public static Menu menu;
 
+    //esto es solo el texto del menu aqui pueden lo que dicen las opciones
+    private final String[] reservaSubMenu = {
+        "<---------------------------------->\n",
+        "         Submenu: Reservas\n",
+        "\t1) Crear nueva reserva\n",
+        "\t2) Cancelar Reserva\n",
+        "\t3) Modificar\n",
+        "\t0) Regresar al menu anterior\n",
+        "<---------------------------------->\n"
+    };
+    private final String[] habitacionSubMenu = {
+        "<---------------------------------->\n",
+        "         Submenu: Habitaciones\n",
+        "\t1) Verificar Habitacion\n",
+        "\t2) Deshabilitar o rehabilitar Habitacion\n",
+        "\t0) Regresar al menu anterior\n",
+        "<---------------------------------->\n"
+    };
+    private final String[] CobroSubMenu = {
+        "<---------------------------------->\n",
+        "        Submenu: cobro\n",
+        "\t1) Pagar Reserva\n",
+        "\t2) Registra algo\n",
+        "\t0) Regresar al menu anterior\n",
+        "<---------------------------------->\n"
+    };
+
     private Menu() {
+        alive = true;
     }
-    
-    public static void Init(){
-        if(menu == null){
+
+    public static void Init() {
+        if (menu == null) {
             menu = new Menu();
-        }
-        else{
+            menu.InitSubmenus();
+        } else {
             System.out.println("El menu ya existe");
         }
     }
-    
-    
-    
+
+    private void InitSubmenus() {
+        submenus[0] = new tabes(reservaSubMenu);
+        submenus[1] = new tabes(habitacionSubMenu);
+        submenus[2] = new tabes(CobroSubMenu);
+    }
+
+    public void Start() {
+        while (alive) {
+            ShowMenu();
+            GaderAnw();
+        }
+
+    }
+
+    private void ShowMenu() {
+        System.out.print("<---------------------------------->\n"
+                + "           MENU PRINCIPAL\n"
+                + "<---------------------------------->\n"
+                + "\t1) Reservaciones\n"
+                + "\t2) Habitaciones\n"
+                + "\t3) Cobrar\n"
+                + "\t0) salir\n"
+                + "<---------------------------------->\n");
+    }
+
+    private void GaderAnw() {
+        opcionSelected = Reader.consola.nextInt();
+        switch (opcionSelected) {
+            case 1:
+                submenus[0].isActive = true;
+                submenus[0].launchSubMenu();
+                break;
+            case 2:
+                submenus[1].isActive = true;
+                submenus[1].launchSubMenu();
+                break;
+            case 3:
+                submenus[2].isActive = true;
+                submenus[2].launchSubMenu();
+                break;
+            case 0:
+                System.out.println("Adios");
+                alive = false;
+            default:
+                System.err.println("Opcion no valida");
+                break;
+        }
+
+    }
+
+    private class tabes {
+
+        public Boolean isActive;
+        private int subOption;
+        private final String[] menuText;
+
+        public tabes(String[] menulines) {
+            menuText = menulines;
+            isActive = true;
+        }
+
+        private void impresor() {
+            for (String printosa1 : menuText) {
+                System.out.println(printosa1);
+            }
+        }
+
+        private void exeSubOption() {
+            subOption = Reader.consola.nextInt();
+            switch (subOption) {
+                case 1:
+                    //aqui se agrega el codigo de lo que hace
+                    System.out.println("La accion se ejecuto");
+                    break;
+                case 2:
+                    //aqui se agrega el codigo de lo que hace
+                    System.out.println("La accion se ejecuto");
+                    break;
+                case 3:
+                    //aqui se agrega el codigo de lo que hace
+                    System.out.println("La accion se ejecuto");
+                    break;
+                case 0:
+                    System.out.println("Adios");
+                    isActive = false;
+                default:
+                    System.err.println("Opcion no valida");
+                    break;
+            }
+        }
+
+        public void launchSubMenu() {
+            while (isActive) {
+                impresor();
+                exeSubOption();
+            }
+        }
+
+    }
+
 }
