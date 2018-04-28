68d4cf7450ab64c0a61f07bbdb16629eba992eef
mcdre <mcdre@DESKTOP-2FMHK21>
2018-04-27 23:25:28 -0600

    creacion de la clase factura

---
+++ src/marilion/Factura.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marilion;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mcdre
+ */
+public class Factura {
+    public Huesped cliente;
+    public float monto;
+    public Date fecha;
+    
+}
