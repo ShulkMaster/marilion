77aab51db81603974724b4fe6e2552451fcddd80
GerryMG <gerardo.andresz4@live.com>
2018-05-04 02:19:03 -0600

    arrreglando gestordecompra

--- src/marilion/GestorCompra.java
+++ src/marilion/GestorCompra.java
@@ -12,29 +12,26 @@ import parserMax.FechaX;
  * @author mcdre
  */
 public class GestorCompra {
+
     /*private int tipoPago;
     private float monto;
     public Persona cliente;
     public boolean Realizado;*/
-    
+
     public static float PrecioDoble;
     public static float PrecioSencilla;
-    
-    
-    public GestorCompra()
-    {
+
+    public GestorCompra() {
         PrecioDoble = 120;
         PrecioSencilla = 70;
-        
+
     }
-    
-    public static float RegresarPrecio(Reservacion re)
-    {
+
+    public static float RegresarPrecio(Reservacion re) {
         float costo = 0;
         float extra = 1.0f;
         float add = 0f;
-        switch(re.tipo)
-        {
+        switch (re.tipo) {
             case basico:
                 add = 10f;
                 break;
@@ -43,32 +40,28 @@ public class GestorCompra {
             case ninguno:
                 add = 0f;
         }
-        for(String str: re.getHIDs())
-        {
-            if(str.charAt(0) == 'e' || str.charAt(0) == 'f' )
-            {
+        for (String str : re.getHIDs()) {
+            if (str.charAt(0) == 'e' || str.charAt(0) == 'f') {
                 extra = 1.1f;
-            }
-            else
-            {
+            } else {
                 extra = 1.0f;
             }
-            if(Character.getNumericValue(str.charAt(1))%2 == 0)
-            {
-                costo += ((PrecioDoble + add) * re.dias)*extra;
-            }
-            else
-            {
-                costo += ((PrecioSencilla + add) * re.dias)*extra;
+            if (str.length() < 3 && Character.getNumericValue(str.charAt(1)) % 2 == 0) {
+                costo += ((PrecioDoble + add) * re.dias) * extra;
+            } else {
+                if (str.length() == 3) {
+                    costo += ((PrecioDoble + add) * re.dias) * extra;
+                } else {
+                    costo += ((PrecioSencilla + add) * re.dias) * extra;
+                }
+
             }
         }
-        
-        
+
         return costo;
     }
-    
-    public static Factura getFactura(Reservacion re)
-    {
-        return new Factura(re.Id_reservacion, re.PersonaAPagar,GestorCompra.RegresarPrecio(re),FechaX.fechaEjemplo());
+
+    public static Factura getFactura(Reservacion re) {
+        return new Factura(re.Id_reservacion, re.PersonaAPagar, GestorCompra.RegresarPrecio(re), FechaX.fechaEjemplo());
     }
 }
